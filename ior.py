13dd033cac848dbac8a55222c22e1cac1f9f49e1
venkasam <65780820+venkasam@users.noreply.github.com>
2021-08-29 23:01:42 -0500

    Add files via upload

---
+++ ior.py
@@ -0,0 +1,18 @@
+import random
+
+class Foo:
+    def __init__(credit, code, val):
+        self.credit = credit
+        self.code=random.randint(1,100)+random.randint(3000,9000)
+    def wi(width):
+        a=self.credit=self.credit-width
+        print(a)
+    def money(now):
+        if (self.credit>0):
+            print("has money")
+    
+     
+
+
+
+  
